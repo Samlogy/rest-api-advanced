292be1e4eb48301cafb337aa93510c0a5479f7db
samlogy <aceportgasd235@gmail.com>
2023-03-27 11:23:21 +0100

    add: user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,36 @@
+import { Schema, model, Document } from "mongoose";
+import { compare, hash } from "bcryptjs";
+
+export interface UserDocument extends Document {
+  email: string;
+  password: string;
+  role: string;
+  createdAt?: any;
+}
+
+const userSchema = new Schema<UserDocument>({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, default: "moderator" },
+  createdAt: { type: Date, default: Date.now() },
+});
+
+userSchema.pre<UserDocument>("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  try {
+    const salt = await hash(this.password, 10);
+    this.password = salt;
+    next();
+  } catch (err: any) {
+    next(err);
+  }
+});
+
+userSchema.methods.comparePasswords = async function (password: string) {
+  return compare(password, this.password);
+};
+
+export const UserModel = model<UserDocument>("User", userSchema);
