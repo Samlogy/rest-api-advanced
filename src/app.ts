838c7bd43216bef33836e50f0eb09a2bbb403c8a
samlogy <aceportgasd235@gmail.com>
2023-04-15 17:27:56 +0100

    fix: error linting

--- src/app.ts
+++ src/app.ts
@@ -1,65 +1,59 @@
-import compression from "compression";
-import express from "express";
-import corsMiddleware from "./middlewares/cors.middleware";
-import deserializeData from "./middlewares/deserializeData.middleware";
-import {
-  globalErrorHandler,
-  notFoundRoute,
-} from "./middlewares/error.middleware";
-import security from "./middlewares/security.middleware";
-import authRoutes from "./routes/auth.route";
+import compression from 'compression'
+import express from 'express'
+import corsMiddleware from './middlewares/cors.middleware'
+import deserializeData from './middlewares/deserializeData.middleware'
+import { globalErrorHandler, notFoundRoute } from './middlewares/error.middleware'
+import security from './middlewares/security.middleware'
+import authRoutes from './routes/auth.route'
 // import { successHandler, errorHandler } from "./utils/logs.utils";
 // import checkApiVersion from "./middlewares/checkApiVersion.middleware";
-import cors from "cors";
+import cors from 'cors'
 
-const allowedOrigins = ["http://example.com", "http://localhost:3000"];
+const allowedOrigins = ['http://example.com', 'http://localhost:3000']
 
 // Define CORS options
 const corsOptions = {
-  origin: (
-    origin: string,
-    callback: (err: Error | null, allow?: boolean) => void
-  ) => {
+  origin: (origin: string, callback: (err: Error | null, allow?: boolean) => void) => {
     if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
+      callback(null, true)
     } else {
-      callback(new Error("Not allowed by CORS"));
+      callback(new Error('Not allowed by CORS'))
     }
-  },
-};
+  }
+}
 
 export default class App {
-  public app: express.Application;
+  public app: express.Application
 
   constructor() {
-    this.app = express();
+    this.app = express()
 
     // set a set of attack preventions --> secure some well know web vulnerabilities OWASP
-    security(this.app);
+    security(this.app)
 
     // // CORS configuration
-    this.app.use(corsMiddleware);
+    this.app.use(corsMiddleware)
 
     // // Logging middleware to track requests
     // this.app.use(successHandler);
     // this.app.use(errorHandler);
 
-    this.app.use(compression());
+    this.app.use(compression())
 
     // // Add the deserialization middleware to the middleware chain
-    this.app.use(deserializeData);
+    this.app.use(deserializeData)
 
     // // Add API version middleware
     // // this.app.use(checkApiVersion);
 
     // // Routes
-    this.app.use("/auth", new authRoutes().router);
+    this.app.use('/auth', new authRoutes().router)
     // // this.app.use("/api/v1/auth", new authRoutes().router);
 
     // // 404 handler middleware to handle requests for non-existent routes
-    this.app.use(notFoundRoute);
+    this.app.use(notFoundRoute)
 
     // // Error handler middleware to handle server errors and return consistent error responses
-    this.app.use(globalErrorHandler);
+    this.app.use(globalErrorHandler)
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,10 @@ import { logger } from './utils/logger'
 // import { redisClient } from './utils/cache.utils'
 
 // Handling uncaught Exception
-// process.on('uncaughtException', (err) => {
-//   logger.info(`Error: ${err.message}`)
-//   logger.info(`shutting down the server for handling uncaught exception`)
-// })
+process.on('uncaughtException', (err) => {
+  logger.info(`Error: ${err.message}`)
+  logger.info(`shutting down the server for handling uncaught exception`)
+})
 
 const app = new App().app
 const PORT = env.PORT
@@ -20,28 +20,28 @@ let server: any
 
 mongoDB(MONGODB_URI)
   .then(() => {
-    // const server = app.listen(PORT, () => {
-    //   logger.info(`App is listening`)
-    // for (let i = 0; i < signals.length; i++) {
-    //   process.on(signals[i], () =>
-    //     gracefulShutdown({
-    //       signal: signals[i],
-    //       server
-    //     })
-    //   )
-    // }
+    const server = app.listen(PORT, () => logger.info(`App is listening`))
+
+    for (let i = 0; i < signals.length; i++) {
+      process.on(signals[i], () =>
+        gracefulShutdown({
+          signal: signals[i],
+          server
+        })
+      )
+    }
     // redisClient
     // })
-    // return server
+    return server
   })
   .catch((err) => console.error(`Failed to connect to database: ${err.message}`))
 
 // unhandled promise rejection
-// process.on('unhandledRejection', (err: Error) => {
-//   logger.info(`Shutting down the server for ${err.message}`)
-//   logger.info(`shutting down the server for unhandle promise rejection`)
+process.on('unhandledRejection', (err: Error) => {
+  logger.info(`Shutting down the server for ${err.message}`)
+  logger.info(`shutting down the server for unhandle promise rejection`)
 
-//   server.close(() => {
-//     process.exit(1)
-//   })
-// })
+  server.close(() => {
+    process.exit(1)
+  })
+})
