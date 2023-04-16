cfc1e1b85ef738ee788f573e376490d82caa9260
samlogy <aceportgasd235@gmail.com>
2023-04-16 11:00:19 +0100

    fix: cors middleware

--- src/app.ts
+++ src/app.ts
@@ -7,20 +7,6 @@ import security from './middlewares/security.middleware'
 import authRoutes from './routes/auth.route'
 // import { successHandler, errorHandler } from "./utils/logs.utils";
 // import checkApiVersion from "./middlewares/checkApiVersion.middleware";
-import cors from 'cors'
-
-const allowedOrigins = ['http://example.com', 'http://localhost:3000']
-
-// Define CORS options
-const corsOptions = {
-  origin: (origin: string, callback: (err: Error | null, allow?: boolean) => void) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
 
 export default class App {
   public app: express.Application
@@ -32,7 +18,7 @@ export default class App {
     security(this.app)
 
     // // CORS configuration
-    this.app.use(corsMiddleware)
+    corsMiddleware(this.app)
 
     // // Logging middleware to track requests
     // this.app.use(successHandler);
--- src/middlewares/cors.middleware.ts
+++ src/middlewares/cors.middleware.ts
@@ -1,59 +1,24 @@
-// import cors from "cors";
-// import { Request } from "express";
+import cors from 'cors'
+import { Application } from 'express'
+const allowedOrigins = ['http://localhost:3000', 'http://yourapp.com']
+const allowedMethods = ['GET', 'POST', 'DELETE', 'PUT', 'PATCH']
 
-// const allowList = ["http://localhost:3000"];
-// const methodsAllowed: string[] = [
-//   "GET",
-//   "POST",
-//   "DELETE",
-//   "UPDATE",
-//   "PUT",
-//   "PATCH",
-// ];
-// const moreOptions = { credentials: true, optionsSuccessStatus: 200 };
-// const options = { methods: methodsAllowed, ...moreOptions };
-
-// const corsManip = (req: Request, callback: any) => {
-//   const origin = req.headers?.origin as string;
-//   console.log("origin: ", origin);
-
-//   // enable requested origin in cors response
-//   if (allowList.indexOf(origin) !== -1)
-//     callback(null, {
-//       origin: true,
-//       ...options,
-//     });
-//   // disable cors in reponse
-//   else
-//     callback(null, {
-//       origin: false,
-//       ...options,
-//     });
-// };
-
-// export default function corsMiddleware() {
-//   cors(corsManip);
-// }
-
-import { Request, Response, NextFunction } from 'express'
-
-const allowedOrigins = ['http://example1.com', 'http://example2.com']
-const methodsAllowed: string[] = ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
-const options = {
-  methods: methodsAllowed,
-  credentials: true,
-  optionsSuccessStatus: 200
+// allow requests with no origin
+// (like mobile apps or curl requests)
+export default function corsMiddleware(app: Application) {
+  app.use(
+    cors({
+      origin: (origin, callback) => {
+        if (!origin) return callback(null, true)
+        if (allowedOrigins.indexOf(origin) === -1) {
+          const msg = 'The CORS policy for this site does not ' + 'allow access from the specified Origin.'
+          return callback(new Error(msg), false)
+        }
+        return callback(null, true)
+      },
+      methods: allowedMethods,
+      credentials: true,
+      optionsSuccessStatus: 200
+    })
+  )
 }
-
-const corsMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  const requestOrigin = req.get('origin')
-  if (requestOrigin && allowedOrigins.includes(requestOrigin)) {
-    res.setHeader('Access-Control-Allow-Origin', requestOrigin)
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-    res.setHeader('Access-Control-Allow-Credentials', 'true')
-  }
-  next()
-}
-
-export default corsMiddleware
