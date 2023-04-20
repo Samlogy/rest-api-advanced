4264722e12b4721f28180c8742eb1c56e35b6ca4
samlogy <aceportgasd235@gmail.com>
2023-04-20 10:23:35 +0100

    edit: graceful function * close redis connection

--- src/utils/gracefulShutdown.ts
+++ src/utils/gracefulShutdown.ts
@@ -1,10 +1,13 @@
+import { signals } from 'constants/signals'
+import Cache from './cache.utils'
 import { disconnectMongoDB } from './db'
 import { logger } from './logger'
-import { signals } from 'constants/signals'
 
 export default async function gracefulShutdown({ signal, server }: { signal: (typeof signals)[number]; server: any }) {
+  const redisClient = new Cache()
   logger.info(`Got signal ${signal}. Good bye`)
   await server.close()
+  await redisClient.close()
   await disconnectMongoDB()
   process.exit(0)
 }
